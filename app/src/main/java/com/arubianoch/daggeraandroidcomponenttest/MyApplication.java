6993313878aef8b28d461578bd3e1bc83d27958c
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-03-27 20:15:40 -0500

    Creating AppComponent on Application class

--- app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/MyApplication.java
+++ app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/MyApplication.java
@@ -6,4 +6,19 @@ import android.app.Application;
  * @author Andres Rubiano Del Chiaro
  */
 public class MyApplication extends Application {
+
+    AppComponent myComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        myComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent getAppComponent() {
+        return myComponent;
+    }
 }
