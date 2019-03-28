3fbe86c028ff120fdf6fcde6cd7d509241879290
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-03-27 20:04:49 -0500

    Created inject methods in AppComponent class

--- app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/AppComponent.java
+++ app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/AppComponent.java
@@ -1,7 +1,16 @@
 package com.arubianoch.daggeraandroidcomponenttest;
 
+import javax.inject.Singleton;
+
+import dagger.Component;
+
 /**
  * @author Andres Rubiano Del Chiaro
  */
+@Singleton
+@Component(modules = {AppModule.class})
 public interface AppComponent {
+
+    void inject(MyApplication application);
+    void inject(MainActivity mainActivity);
 }
