219b9a18468c436435f3131f6fbdb5b2d7d88003
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-03-27 20:43:35 -0500

    Injecting Classes to be used in MainActivity class

--- app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/MainActivity.java
+++ app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/MainActivity.java
@@ -1,13 +1,31 @@
 package com.arubianoch.daggeraandroidcomponenttest;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    SharedPreferences preferences;
+
+    @Inject
+    Context context;
+
+    @Inject
+    Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((MyApplication) getApplication()).getAppComponent().inject(this);
+
+        preferences.edit().putInt("Num", 1).apply();
     }
 }
