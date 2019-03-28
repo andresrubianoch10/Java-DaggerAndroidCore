d476cc19b9a901e673741770d6db48abec767ddc
Andres Rubiano Del Chiaro <andres.rubiano.ch@gmail.com>
2019-03-27 20:02:33 -0500

    Added Module providers methods with MyApplication, Context and SharedPreferences.

--- app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/AppModule.java
+++ app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/AppModule.java
@@ -1,7 +1,40 @@
 package com.arubianoch.daggeraandroidcomponenttest;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
 /**
  * @author Andres Rubiano Del Chiaro
  */
+@Module
 public class AppModule {
+
+    private final MyApplication myApplication;
+
+    public AppModule(MyApplication myApplication) {
+        this.myApplication = myApplication;
+    }
+
+    @Provides
+    @Singleton
+    Context provideAppContext() {
+        return myApplication;
+    }
+
+    @Provides
+    @Singleton
+    SharedPreferences provideSharedPreferences(Context context) {
+        return context.getSharedPreferences("pref", Context.MODE_PRIVATE);
+    }
+
+    @Provides
+    Resources getResources(Context context) {
+        return context.getResources();
+    }
 }
--- app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/MyApplication.java
+++ app/src/main/java/com/arubianoch/daggeraandroidcomponenttest/MyApplication.java
@@ -1,7 +1,9 @@
 package com.arubianoch.daggeraandroidcomponenttest;
 
+import android.app.Application;
+
 /**
  * @author Andres Rubiano Del Chiaro
  */
-public class MyApplication {
+public class MyApplication extends Application {
 }
